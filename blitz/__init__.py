81d8d68d8e1282c4e66ab1fb3dfe1e428657c7c4
maybe-raymond <mugandiwaraymond@gmail.com>
2020-08-17 01:40:21 +0200

    fixed bug

--- blitz/__init__.py
+++ blitz/__init__.py
@@ -15,8 +15,6 @@ app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///database.db"
 app.config["MAIL_SERVER"]= "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_TLS"]= False
-app.config["MAIL_USERNAME"] = "mugandiwaraymond@gmail.com"
-app.config["MAIL_PASSWORD"] = "Dogbatman10120"
 app.config["MAIL_USE_SSL"] = True
 
 
